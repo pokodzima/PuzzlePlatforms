f4ca59bcee811c943bdee26afb7e9831d664a66d
pokodzima <52672768+pokodzima@users.noreply.github.com>
2023-01-06 03:45:09 +0600

    Moving Platform

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (!HasAuthority()) return;
+	
+	FVector Location = GetActorLocation();
+	Location += FVector(Speed * DeltaSeconds, 0, 0);
+	SetActorLocation(Location);
+}
